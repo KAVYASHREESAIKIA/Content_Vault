3c4923982c66aa50456bc619f68859aa15c50d7d
KAVYASHREESAIKIA <132487925+KAVYASHREESAIKIA@users.noreply.github.com>
2026-02-11 22:05:04 +0530

    Delete Content_Vault directory

--- Content_Vault/config/init.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-try {
-  mongoose.connect(process.env.MONGO);
-  console.log("Connected to Mongo Successfully!");
-} catch (error) {
-  console.log(error.message);
-}
